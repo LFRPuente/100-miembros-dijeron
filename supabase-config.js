e39fa2a6905082cdb7d95b2e63a2747da2c33391
Luis <luis@users.noreply.github.com>
2026-07-18 01:53:47 -0600

    Connect shared question bank to Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@
   "use strict";
 
   global.SUPABASE_CONFIG = Object.freeze({
-    url: "",
-    publishableKey: ""
+    url: "https://djvhmnftdbyxrqwgyjkd.supabase.co",
+    publishableKey: "sb_publishable_1hJRqmLI4uF0FwRbewZ-8Q_RMVtZWbJ"
   });
 })(window);
